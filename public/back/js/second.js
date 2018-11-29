61b6e9c8aa0f27c55e2eac48350826550ac2a7a4
jepson <jepsonpp@qq.com>
2018-11-29 18:23:55 +0800

    下拉列表渲染完成, 图片上传完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -7,6 +7,7 @@ $(function() {
 
   var currentPage = 1; // 当前页
   var pageSize = 5; // 每页多少条
+  // 1. 渲染页面
   render();
 
   function render() {
@@ -42,6 +43,56 @@ $(function() {
       }
     })
   }
+
+
+
+  // 2. 点击添加分类按钮, 显示添加模态框
+  $('#addBtn').click(function() {
+    $('#addModal').modal("show");
+
+    // 发送请求, 获取一级分类的全部数据
+    // 通过写死 page 和 pageSize 模拟获取全部一级分类的接口
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100,
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info )
+        var htmlStr = template("dropdownTpl", info);
+        $('.dropdown-menu').html( htmlStr );
+      }
+    })
+  });
+
+
+
+  // 3. 给下拉列表的 a 添加点击事件(通过事件委托实现)
+  $('.dropdown-menu').on("click", 'a', function() {
+    // 获取文本值
+    var txt = $(this).text();
+    // 设置给按钮
+    $('#dropdownText').text( txt );
+  });
+
+
+  // 4. 配置文件上传插件, 让插件发送异步文件上传请求
+  $('#fileupload').fileupload({
+    dataType: "json",
+    // done 表示文件上传完成的回调函数
+    done: function( e, data ) {
+      //console.log( data );
+      //console.log( data.result ); // 后台返回的对象
+
+      var picObj = data.result; // 后台返回的数据
+      // 获取图片地址, 设置给 img src
+      var picUrl = picObj.picAddr;
+      $('#imgBox img').attr("src", picUrl);
+    }
+  })
 
 
 })
